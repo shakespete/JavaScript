89e64712383f3b3402d14630b95185aaf49598b0
shakespete <petersyjuco@gmail.com>
2020-11-23 09:17:05 +1100

    Inits sherlock and the beast problem

---
+++ algorithms/decent_number.js
@@ -0,0 +1,45 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+// Complete the decentNumber function below.
+function decentNumber(n) {}
+
+function main() {
+  const t = parseInt(readLine().trim(), 10);
+
+  for (let tItr = 0; tItr < t; tItr++) {
+    const n = parseInt(readLine().trim(), 10);
+
+    decentNumber(n);
+  }
+}
+
+// 4
+// 1
+// 3
+// 5
+// 11
+
+// -1
+// 555
+// 33333
+// 55555533333
