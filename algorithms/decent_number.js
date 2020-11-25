88ac043c782a508c91218e5d7c4ef26975f06e1b
shakespete <petersyjuco@gmail.com>
2020-11-25 17:49:44 +1100

    WIP: backtrack approach

--- algorithms/decent_number.js
+++ algorithms/decent_number.js
@@ -11,18 +11,38 @@ function decentNumber(n) {
   let qty5 = Math.floor(n / 5);
 
   if (mod3 === 0) {
-    console.log(qty3, mod3);
-    for (let i = 1; i <= qty3; ++i) {
-      ans += str3;
-    }
+    for (let i = 1; i <= qty3; ++i) ans += str3;
   } else if (mod5 === 0) {
-    console.log(qty5, mod5);
-    for (let i = 1; i <= qty5; ++i) {
-      ans += str5;
-    }
+    for (let i = 1; i <= qty5; ++i) ans += str5;
+  } else if (n >= 3) {
+    let decreasingN = n;
+    while (decreasingN %)
+
+    ans += str3;
+    decreasingN - 3;
+
   } else {
+    ans = '-1';
   }
   console.log(ans);
+}
+
+const greedy = (str = '', N) => {
+  if (N === 0) return str;
+  else if (N >= 3) {
+    let currStr = str;
+    let currN = N;
+
+    greedy(currStr += '555', N - 3);
+    // Need to backtrack!
+    greedy(currStr += '33333', N - 5);
+  } else {
+    return '-1';
+  }
+} 
+
+function decentNumber(n) {
+  greedy('', n);
 }
 
 // 4
